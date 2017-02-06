7e419ffe185897e9a58e2da63120d007dd0c4b80
hackergirl1 <hackergirl1@users.noreply.github.com>
2017-02-06 12:17:28 -0400

    linear search implemented

--- Lab1.cpp
+++ Lab1.cpp
@@ -8,14 +8,17 @@ int linearSearch(auto data, auto key);//prototype
 int linearSearch(auto data, auto key){
 for (int location=0;location<data.size();location++)
 {
-        if (data[location]==key)
+        if (data[location].get_grade() == key)
         return location;
 }
 return -1; //not found
 }
 
-int main(){
+int main()
+{
 vector<Grades> v;
+char search_key, choice;
+int result;
 Grades obj;
 obj.set_grade('A');
 obj.set_mark(85);
@@ -30,9 +33,34 @@ obj.set_grade('D');
 obj.set_mark(50);
 v.push_back(obj);
 
+cout<<"Enter a grade (e.g. A, B) to be searched :)"<<endl;
+cout<<"To end input, enter the character '#'"<<endl;
+cin>>search_key;
+while(search_key != '#')
+    {
+        result = linearSearch(v, search_key);
+
+        cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+
+
+        cout<<endl<<endl<<"Enter a value to search for: ";
+        cin>>search_key;
+    }
+cout<<"Before you go, would you like to print the grades vector? y/n ";
+cin>>choice;
+if (choice =='y'){ 
 for(int i=0; i<v.size();i++){
 cout<<v[i].get_grade()<<""
     <<v[i].get_mark()<<endl;
+}
+}
+else{
+cout<<endl<<"Have a nice day!!! ;)"<<endl;
 }
 return 0;
 }
